6256d7f90f261a7badc0ad03953791163e3ff3a0
EC2 Default User <ec2-user@ip-10-0-0-101.ap-northeast-1.compute.internal>
2017-02-08 17:56:04 +0900

    タグ作成・検索実装

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,7 +9,7 @@ class TagsController < ApplicationController
 
   # GET /tags/1
   # GET /tags/1.json
-  def show
+  def detail
   end
 
   # GET /tags/new
@@ -28,8 +28,8 @@ class TagsController < ApplicationController
 
     respond_to do |format|
       if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render :show, status: :created, location: @tag }
+        format.html { redirect_to @tag, notice: 'タグを作成しました。' }
+        format.json { render :detail, status: :created, location: @tag }
       else
         format.html { render :new }
         format.json { render json: @tag.errors, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class TagsController < ApplicationController
   def update
     respond_to do |format|
       if @tag.update(tag_params)
-        format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
+        format.html { redirect_to @tag, notice: 'タグを作成しました。' }
         format.json { render :show, status: :ok, location: @tag }
       else
         format.html { render :edit }
@@ -56,9 +56,14 @@ class TagsController < ApplicationController
   def destroy
     @tag.destroy
     respond_to do |format|
-      format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
+      format.html { redirect_to tags_url, notice: 'タグを削除しました。' }
       format.json { head :no_content }
     end
+  end
+
+  # GET /tags/search
+  def search
+    @tags = Tag.search(params[:search])
   end
 
   private
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,10 @@
 class Tag < ApplicationRecord
+	validates :name, length: {maximum: 30}
+	def self.search(search) #self.でクラスメソッドとしている
+		if search
+			Tag.where(['name LIKE ?', "%#{search}%"])
+		else
+			Tag.all
+		end
+	end
 end
