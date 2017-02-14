bebbc5472a2bbe151a305e42398ba57bd7593182
EC2 Default User <ec2-user@ip-10-0-0-101.ap-northeast-1.compute.internal>
2017-02-14 20:56:13 +0900

    user_tag追加

---
+++ db/migrate/20170213022600_create_user_tags.rb
@@ -0,0 +1,13 @@
+class CreateUserTags < ActiveRecord::Migration[5.0]
+  def change
+    create_table :user_tags do |t|
+      t.integer :user_id
+      t.integer :tag_id
+
+      t.timestamps
+    end
+    add_index :user_tags, :user_id
+    add_index :user_tags, :tag_id
+    add_index :user_tags, [:user_id, :tag_id], unique: true
+  end
+end
