7e84dbd79059b0f04fbff123cc2398ef2625ef87
Vitor Nagao <vitornb13@hotmail.com>
2017-04-12 20:51:52 +0900

    add mailer Class

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,3 +1,15 @@
+class EventMailer < ApplicationMailer
 
-class 
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.event_mailer.send.subject
+  #
+  def send(user, event)
+    @user = user
+    @event = event
+    mail to: @user.email, subject: 'ビオトープイベント開催のお知らせ'
+
+    # mail to: "to@example.org"
+  end
 end
